1409ba832e06a47bef8806dbf2bbbf05eb9a2a8e
Phi Long <91886384+PhiLongVN@users.noreply.github.com>
2021-12-23 20:20:00 +0700

    upload

---
+++ script/script.js
@@ -0,0 +1,80 @@
+const bill = document.querySelector(".bill");
+const people = document.querySelector(".people");
+const selects = document.querySelectorAll(".percente");
+const custom = document.querySelector(".custom");
+const tip = document.querySelector(".tip");
+const total = document.querySelector(".total-am");
+const reset = document.querySelector(".reset");
+let selectVL;
+let billVL;
+let peopleVL;
+let customVL;
+
+selects.forEach((select) => {
+  select.addEventListener("click", selectButton);
+});
+bill.addEventListener("input", handleBill);
+people.addEventListener("input", handlePeople);
+custom.addEventListener("input", hadleCustom);
+
+function selectButton(e) {
+  selects.forEach((select) => {
+    if (e.target.value == select.value) {
+      select.classList.toggle("active");
+      selectVL = select.value / 100;
+    } else {
+      select.classList.remove("active");
+    }
+  });
+  caculate();
+  //   console.log("selectButton -> selectVL", selectVL);
+}
+
+function handleBill(e) {
+  billVL = e.target.value;
+
+  caculate();
+}
+
+function handlePeople(e) {
+  peopleVL = e.target.value;
+  if (peopleVL > 0 || peopleVL == "") {
+    e.target.parentNode.classList.remove("active");
+    caculate();
+  } else {
+    e.target.parentNode.classList.add("active");
+  }
+}
+
+function caculate() {
+  const a = document.querySelector(".tip-amount p:nth-of-type(2)");
+  const b = document.querySelector(".total p:nth-of-type(2)");
+  //   console.log("caculate -> b", b);
+
+  let tip2 = (billVL / peopleVL) * selectVL;
+  a.innerHTML = `$${tip2.toFixed(2)}`;
+  let total2 = (billVL / peopleVL) * selectVL + billVL / peopleVL;
+  b.innerHTML = `$${total2.toFixed(2)}`;
+
+  if (customVL) {
+    let tip2 = (billVL / peopleVL) * customVL;
+    a.innerHTML = `$${tip2.toFixed(2)}`;
+    let total2 = (billVL / peopleVL) * customVL + billVL / peopleVL;
+    b.innerHTML = `$${total2.toFixed(2)}`;
+  }
+}
+
+reset.onclick = () => {
+  const a = document.querySelector(".tip-amount p:nth-of-type(2)");
+  const b = document.querySelector(".total p:nth-of-type(2)");
+
+  a.innerHTML = "$0.00";
+  b.innerHTML = "$0.00";
+  bill.value = '0'
+  people.value = '0'
+};
+
+function hadleCustom(e) {
+  customVL = (e.target.value)/100;
+  caculate();
+}
